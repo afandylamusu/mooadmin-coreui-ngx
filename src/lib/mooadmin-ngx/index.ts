66a28a86152ef9f086bf8af56b3eecca3b3cea37
sjafrudin.lamusu <sjafrudin.lamusu@moonlay.com>
2018-08-21 10:14:34 +0700

    Add mooadmin-ngx/index.ts

---
+++ src/lib/mooadmin-ngx/index.ts
@@ -0,0 +1,10 @@
+
+export * from './dictionary';
+export * from './active-record';
+export * from './field-types';
+export { MooadminModule } from './mooadmin.module';
+export { MooFieldComponent } from './moo-field/moo-field.component';
+export { MooVformComponent } from './moo-vform/moo-vform.component';
+export { MooVlistComponent } from './moo-vlist/moo-vlist.component';
+export { MooVtableComponent } from './moo-vtable/moo-vtable.component';
+export { MooVchartComponent } from './moo-vchart/moo-vchart.component';
